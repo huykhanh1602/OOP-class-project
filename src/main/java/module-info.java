c999a6dea1979073b9d05e4177dd24d83ade1c4f
NosDrg <long2k6tb@gmail.com>
2025-10-05 13:06:57 +0700

    demo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module vnu.edu.vn {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens vnu.edu.vn to javafx.fxml;
     exports vnu.edu.vn;
--- src/main/java/vnu/edu/vn/App.java
+++ src/main/java/vnu/edu/vn/App.java
@@ -1,38 +1,32 @@
 package vnu.edu.vn;
 
+/// Launcher game
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import vnu.edu.vn.game.GameScene;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
+    //WINDOW SETTING
+    public final int widthScreen = 800;             //WIDTH SCREEN
+    public final int heightScreen = 600;            //HEIGHT SCREEN
+    private final String title = "Arknoid";
 
-    private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+
+        GameScene gameScene = new GameScene(widthScreen, heightScreen);
+        stage.setTitle(title);
+        stage.setScene(gameScene.getScene());
         stage.show();
+
+        gameScene.start();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+    //LAUNCH GAME
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
