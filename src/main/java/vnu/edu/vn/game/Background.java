f1aca0e9f0dcb2d8434cb28324edd45ea00f88cb
NosDrg <125871674+NosDrg@users.noreply.github.com>
2025-10-07 22:19:31 +0700

    add draw background and anything

---
+++ src/main/java/vnu/edu/vn/game/Background.java
@@ -0,0 +1,47 @@
+package vnu.edu.vn.game;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Background {
+    private List<Image> backgrounds = new ArrayList<>();
+    private int currentIndex = 0;
+    private int widthMap, heightMap;
+
+    public Background(int widthMap, int heightMap) {
+        this.widthMap = widthMap;
+        this.heightMap = heightMap;
+
+        try {
+            backgrounds.add(new Image(getClass().getResourceAsStream("/images/bg1.png")));
+            backgrounds.add(new Image(getClass().getResourceAsStream("/images/bg2.png")));
+            backgrounds.add(new Image(getClass().getResourceAsStream("/images/bg3.png")));
+        } catch (Exception e) {
+            System.out.println("Don't find any images");
+        }
+    }
+
+    public void render(GraphicsContext gc) {
+        if (!backgrounds.isEmpty()) {
+            gc.drawImage(backgrounds.get(currentIndex), 0, 0, widthMap, heightMap);
+        } else {
+            gc.setFill(Color.LIGHTGRAY);
+            gc.fillRect(0, 0, widthMap, heightMap);
+        }
+    }
+
+    public void nextBackground() {
+        currentIndex = (currentIndex + 1) % backgrounds.size();
+    }
+
+    public void setBackground(int index) {
+        if (index >= 0 && index < backgrounds.size()) {
+            currentIndex = index;
+        }
+    }
+}
+
