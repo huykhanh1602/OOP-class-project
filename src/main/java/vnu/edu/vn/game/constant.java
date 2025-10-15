cc0a9350f5321718dc9c7acae98c3cae0e52737c
huykhanh1602 <24020179@vnu.edu.vn>
2025-10-15 14:12:29 +0700

    add texture of coin, ball, destroy stage of brick

---
+++ src/main/java/vnu/edu/vn/game/constant.java
@@ -0,0 +1,25 @@
+package vnu.edu.vn.game;
+
+public class constant {
+    // Screen settings
+    public static final int WIDTH_SCREEN = 800;
+    public static final int HEIGHT_SCREEN = 600;
+
+    // paddle settings
+    public static int PADDLE_SPEED = 10;
+    public static int PADDLE_WIDTH = 100;
+    public static final int PADDLE_HEIGHT = 20;
+
+    // ball settings
+    public static int BALL_RADIUS = 10;
+    public static int INITIAL_BALL_SPEED = 5;
+    public static final int BRICK_ROWS = 9;
+    public static final int BRICK_COLUMNS = 10;
+
+    // brick settings
+    public static final int BRICK_WIDTH = 50;
+    public static final int BRICK_HEIGHT = 50;
+
+    //path
+    public static final
+}
