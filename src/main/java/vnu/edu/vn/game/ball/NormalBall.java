4bb4c843f97c1bc57298643ecbe1890f5605d68d
HoangDinhKien06 <keniss3478@gmail.com>
2025-10-16 19:08:16 +0700

    Merge branch 'fix-lỗi-linh-tinh' of C:\Code\OOP-class-project with conflicts.

---
+++ src/main/java/vnu/edu/vn/game/ball/NormalBall.java
@@ -0,0 +1,8 @@
+package vnu.edu.vn.game.ball;
+
+public class NormalBall extends Ball {
+    public NormalBall(double x, double y) {
+        super(x, y); // Giả sử lớp Ball có constructor nhận x và y
+    }
+
+}
