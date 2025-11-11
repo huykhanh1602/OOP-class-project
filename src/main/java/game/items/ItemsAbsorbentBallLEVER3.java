dc904f1ba02e66fa8e704fb92c552e0fbb2776e8
HoangDinhKien06 <keniss3478@gmail.com>
2025-11-11 13:07:09 +0700

    add image for items

---
+++ src/main/java/game/items/ItemsAbsorbentBallLEVER3.java
@@ -0,0 +1,29 @@
+package game.items;
+
+import game.abstraction.Ball;
+import game.abstraction.Bricks;
+
+import java.util.List;
+
+public class ItemsAbsorbentBallLEVER3 extends ItemsForBall {
+    public ItemsAbsorbentBallLEVER3() {
+        super("Bóng Hấp Thụ(Cấp 5)","Hiệu lực 10s. Khi nhặt: tạo 1 bóng mới trên sân. Với mỗi quả bóng"+
+                " va chạm sẽ tăng damege, tốc độ, size cho mọi quả bóng",10,2);
+    }
+    @Override
+    public void onBrickCollision(Ball collidingBall, List<Ball> allBalls, List<Bricks> allBricks, List<Ball> pendingBalls) {
+        for (Ball ball : allBalls) {
+            double currentSpeed = ball.getSpeedball();
+            double currentDamege = ball.getDamage();
+            double currentSize = ball.getRadius();
+            ball.setSpeedball(currentSpeed * 1.002);
+            ball.setDamage(currentDamege * 1.004);
+            ball.setRadius(currentSize * 1.004);
+        }
+    }
+    @Override
+    public void onFallingCollision(Ball collidingBall, List<Ball> allBalls, List<Bricks> allBricks, List<Ball> pendingBalls){
+        List<Ball> newBalls = this.shatter(collidingBall);
+        pendingBalls.addAll(newBalls);
+    }
+}
---
+++ src/main/java/game/items/ItemsCloneBallLEVER1.java
@@ -0,0 +1,17 @@
+package game.items;
+
+import game.abstraction.Ball;
+import game.abstraction.Bricks;
+import java.util.List;
+import java.util.Random;
+public class ItemsCloneBallLEVER1 extends ItemsForBall {
+    private static final Random random = new Random();
+    public ItemsCloneBallLEVER1() {
+        super("Bóng Nhân Bản(Cấp 1)", "Khi nhặt:Tạo 1 bóng mới trên thanh chắn", 10, 50);
+    }
+    @Override
+    public void onFallingCollision(Ball collidingBall, List<Ball> allBalls, List<Bricks> allBricks, List<Ball> pendingBalls){
+        List<Ball> newBalls = this.shatter(collidingBall);
+        pendingBalls.addAll(newBalls);
+    }
+}
